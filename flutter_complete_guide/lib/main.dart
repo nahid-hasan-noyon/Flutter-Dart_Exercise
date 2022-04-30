d417fa77f3e17c564e8db6c3cc724bcbae93b146
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-04-30 12:19:25 +0600

    connecting everything to main file

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,25 +1,51 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import './question.dart';
-void main() {
-  runApp(const MyApp());
-}
+import './quiz.dart';
+import 'result.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
-    return _MyAppstate();     //? adding "_" changes that thing to private that can't be accessed from the outside of that current file.
+    return _MyAppstate(); //? adding "_" changes that thing to private that can't be accessed from the outside of that current file.
   }
 }
 
 class _MyAppstate extends State<MyApp> {
   var _questionIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ["Red", 'Green', 'Blue', "White"],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ["Ape", 'Snake', 'Lion', "Tiger"],
+    },
+    {
+      'questionText': 'What\'s your favorite fruit?',
+      'answers': ["Apple", 'Banana', 'Mango', "Papaya"],
+    },
+    {
+      'questionText': 'What\'s your favorite flower?',
+      'answers': ["Sunflower", 'Lotus', 'Rose', "Belly"],
+    }
+  ];
 
   // A Named function
-  void buttonWasClicked() {
+  void _answerQuestion() {
+    if (_questionIndex < _questions.length) {
+      if (kDebugMode) {
+        print('printing');
+      }
+    }
     setState(() {
       // rerender / change the UI
       _questionIndex += 1;
@@ -32,73 +58,20 @@ class _MyAppstate extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favorite color?',
-      'What\'s your favorite animal?'
-    ];
     return MaterialApp(
       home: Scaffold(
-          //Creates a visual scaffold for material design widgets
-          appBar: AppBar(
-            //
-            title: const Text('Quizy'),
-          ),
-          body: Column(
-            //? body can hold only one widget
-            // column is used to use multiple widgets
-            children: [
-                Question(    // Text(
-                questions[_questionIndex],
-              ),
-
-              //*  adding buttons
-              // first way
-              const RaisedButton(
-                // striped widgets means these should be used but can be in this version.
-                child: Text('Answer 1'),
-                onPressed: null, // null makes the button disable
-              ),
-              // second way
-              const ElevatedButton(
-                onPressed: null,
-                child: Text('Answer 2'),
-              ),
-
-              //* button that do something on a click.
-              // 1. Using named function
-              RaisedButton(
-                child: const Text('Answer 3'),
-                onPressed: buttonWasClicked,
-                // ! Function have to be called without ().
-              ),
-              ElevatedButton(
-                child: const Text('Answer 4'),
-                onPressed: buttonWasClicked,
-              ),
-
-              // 2. Using anonymous function
-              RaisedButton(
-                child: const Text('Answer 5'),
-                onPressed: () {
-                  if (kDebugMode) {
-                    print('Answer 5 was clicked');
-                  }
-                },
-              ),
-              ElevatedButton(
-                child: const Text('Answer 6'),
-                onPressed: () {
-                  if (kDebugMode) {
-                    print('Answer 6 was clicked');
-                  }
-                },
-              ),
-              RaisedButton(
-                child: const Text('change the question'),
-                onPressed: buttonWasClicked,
-              ),
-            ],
-          )),
+        //Creates a visual scaffold for material design widgets
+        appBar: AppBar(
+          title: const Text('Quizy'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : const Result(),
+      ),
     );
   }
 }
