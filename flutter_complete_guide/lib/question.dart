8801fea3f1c60f709152f0e9e9f107a54142722b
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-04-30 12:17:47 +0600

    spliting the questions section

--- flutter_complete_guide/lib/question.dart
+++ flutter_complete_guide/lib/question.dart
@@ -1,13 +1,21 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
+  final String questionText; //? finial is used to say that
 
-  final String questionText;  //? finial is used to say that 
-
-  const Question(this.questionText, {Key? key}) : super(key: key); 
+  const Question(this.questionText, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      margin: const EdgeInsets.all(10),
+      width: double.infinity, // gets the full size of screen
+      child: Text(
+        questionText, // string which will come via constructor
+        style: const TextStyle(fontSize: 28),
+        textAlign: TextAlign.center, //? TextAlign.center is a enum
+        //? enum is a list of predefined values.
+      ),
+    );
   }
 }
