29fcc9c1e57e536d9ec4f62b410478a699da8694
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-04 15:37:03 +0600

    breaking the code

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -1,10 +1,26 @@
-import 'package:expense_planner/widgets/user_transaction.dart';
+import 'package:expense_planner/widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
+
+import 'models/transaction.dart';
+import 'widgets/transaction_list.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void displayAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return NewTransaction();
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,40 +29,69 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 700, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Dress', amount: 600, date: DateTime.now()),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('Expanse Planner')),
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const <Widget>[
-                // * Chart
-                Card(
-                  color: Colors.amber,
-                  child: SizedBox(
-                    width: double.infinity,
-                    child: Center(child: Text('Chart!')),
-                  ),
-                  elevation: 5,
-                ),
-                UserTransaction(),
-                // * List of Transactions
-              ],
-            ),
+      appBar: AppBar(
+        title: const Text('Expanse Planner'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
           ),
-        ));
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const <Widget>[
+              // * Chart
+              Card(
+                color: Colors.amber,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: Center(child: Text('Chart!')),
+                ),
+                elevation: 5,
+              ),
+              TransactionList(_userTransactions),
+              // * List of Transactions
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
