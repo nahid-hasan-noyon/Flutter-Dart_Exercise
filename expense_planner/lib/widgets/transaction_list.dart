c227fea38449a128b846bfd5c333de0ca4fa2008
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-04 14:30:29 +0600

    scrollable list with ListView.builder

--- expense_planner/lib/widgets/transaction_list.dart
+++ expense_planner/lib/widgets/transaction_list.dart
@@ -9,48 +9,53 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Card(
-            color: Colors.white70,
-            elevation: 5,
-            child: Row(
-              children: [
-                // * Amount
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  )),
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    '\$${tx.amount}',
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Colors.green),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // * Transaction title
-                    Text(
-                      tx.title,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
+    return SizedBox(
+      height: 430,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+              color: Colors.white70,
+              elevation: 5,
+              child: Row(
+                children: [
+                  // * Amount
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      '\$${transactions[index].amount}',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.green),
                     ),
-                    // * Transaction date
-                    Text(
-                      DateFormat('dd-MM-yyyy').format(tx.date),
-                      style: const TextStyle(color: Colors.grey),
-                    )
-                  ],
-                ),
-              ],
-            ));
-      }).toList(),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // * Transaction title
+                      Text(
+                        transactions[index].title,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      // * Transaction date
+                      Text(
+                        DateFormat('dd-MM-yyyy')
+                            .format(transactions[index].date),
+                        style: const TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  ),
+                ],
+              ));
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
