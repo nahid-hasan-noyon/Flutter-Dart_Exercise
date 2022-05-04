3166aa76a8841f5bd95bab23a7efdba106b71434
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-05 00:10:41 +0600

    image will appear when there is no transaction,

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -45,10 +45,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 700, date: DateTime.now()),
-    Transaction(
-        id: 't2', title: 'New Dress', amount: 600, date: DateTime.now()),
+    // Transaction(
+    //     id: 't1', title: 'New Shoes', amount: 700, date: DateTime.now()),
+    // Transaction(
+    //     id: 't2', title: 'New Dress', amount: 600, date: DateTime.now()),
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
--- expense_planner/lib/widgets/transaction_list.dart
+++ expense_planner/lib/widgets/transaction_list.dart
@@ -11,51 +11,63 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 430,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-              color: Colors.white70,
-              elevation: 5,
-              child: Row(
-                children: [
-                  // * Amount
-                  Container(
-                    margin: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Theme.of(context).primaryColorDark,
-                      width: 2,
-                    )),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      '\$${transactions[index].amount.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                          color: Colors.green),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // * Transaction title
-                      Text(
-                        transactions[index].title,
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      // * Transaction date
-                      Text(
-                        DateFormat('dd-MM-yyyy')
-                            .format(transactions[index].date),
-                        style: const TextStyle(color: Colors.grey),
-                      )
-                    ],
-                  ),
-                ],
-              ));
-        },
-        itemCount: transactions.length,
-      ),
+      child: transactions.isEmpty
+          ? Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text(
+                  'No Transactions added yet',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Image.asset('assets/images/no_transactions.png')
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                    color: Colors.white70,
+                    elevation: 5,
+                    child: Row(
+                      children: [
+                        // * Amount
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            color: Theme.of(context).primaryColorDark,
+                            width: 2,
+                          )),
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            '\$${transactions[index].amount.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.green),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // * Transaction title
+                            Text(
+                              transactions[index].title,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            // * Transaction date
+                            Text(
+                              DateFormat('dd-MM-yyyy')
+                                  .format(transactions[index].date),
+                              style: const TextStyle(color: Colors.grey),
+                            )
+                          ],
+                        ),
+                      ],
+                    ));
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
