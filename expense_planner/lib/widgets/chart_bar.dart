d0d38bab0ed04237a3419dc16a729115700d1823
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-11 09:30:35 +0600

    adding weekday letter below chart bars and scaling the chart bar and amount label

--- expense_planner/lib/widgets/chart_bar.dart
+++ expense_planner/lib/widgets/chart_bar.dart
@@ -12,8 +12,11 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(
-          child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 20,
+          child: FittedBox(
+            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+          ),
         ),
         const SizedBox(
           height: 4,
@@ -39,7 +42,11 @@ class ChartBar extends StatelessWidget {
               ),
             )
           ]),
-        )
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Text(label),
       ],
     );
   }
