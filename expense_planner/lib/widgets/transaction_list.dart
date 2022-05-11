1241e6e192878c83feea2dfd0185459acfd39136
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-11 09:16:33 +0600

    improving by ListTile

--- expense_planner/lib/widgets/transaction_list.dart
+++ expense_planner/lib/widgets/transaction_list.dart
@@ -25,49 +25,76 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (context, index) {
                 return Card(
-                    color: Colors.white70,
-                    elevation: 5,
-                    child: Row(
-                      children: [
-                        // * Amount
-                        Container(
-                          margin: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              border: Border.all(
-                            color: Theme.of(context).primaryColorDark,
-                            width: 2,
-                          )),
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            '\$${transactions[index].amount.toStringAsFixed(2)}',
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: Colors.green),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // * Transaction title
-                            Text(
-                              transactions[index].title,
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            // * Transaction date
-                            Text(
-                              DateFormat('dd-MM-yyyy')
-                                  .format(transactions[index].date),
-                              style: const TextStyle(color: Colors.grey),
-                            )
-                          ],
-                        ),
-                      ],
-                    ));
+                  elevation: 5,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    // exchange of card which is below commented
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: FittedBox(
+                            child: Text('\$${transactions[index].amount}')),
+                      ),
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      // style:,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                    ),
+                  ),
+                );
               },
               itemCount: transactions.length,
             ),
     );
   }
 }
+
+// Card(
+//                     color: Colors.white70,
+//                     elevation: 5,
+//                     child: Row(
+//                       children: [
+//                         // * Amount
+//                         Container(
+//                           margin: const EdgeInsets.all(10),
+//                           decoration: BoxDecoration(
+//                               border: Border.all(
+//                             color: Theme.of(context).primaryColorDark,
+//                             width: 2,
+//                           )),
+//                           padding: const EdgeInsets.all(10),
+//                           child: Text(
+//                             '\$${transactions[index].amount.toStringAsFixed(2)}',
+//                             style: const TextStyle(
+//                                 fontWeight: FontWeight.bold,
+//                                 fontSize: 16,
+//                                 color: Colors.green),
+//                           ),
+//                         ),
+//                         Column(
+//                           crossAxisAlignment: CrossAxisAlignment.start,
+//                           children: [
+//                             // * Transaction title
+//                             Text(
+//                               transactions[index].title,
+//                               style:
+//                                   const TextStyle(fontWeight: FontWeight.bold),
+//                             ),
+//                             // * Transaction date
+//                             Text(
+//                               DateFormat('dd-MM-yyyy')
+//                                   .format(transactions[index].date),
+//                               style: const TextStyle(color: Colors.grey),
+//                             )
+//                           ],
+//                         ),
+//                       ],
+//                     ));
+//               },
