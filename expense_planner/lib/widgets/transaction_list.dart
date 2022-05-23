c9bf29f4877dd33b1e30d87cc7f2aadc19ff472a
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-24 01:20:14 +0600

    spliting the code

--- expense_planner/lib/widgets/transaction_list.dart
+++ expense_planner/lib/widgets/transaction_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
+import '../widgets/transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -34,70 +34,15 @@ class TransactionList extends StatelessWidget {
           )
         : ListView.builder(
             itemBuilder: (context, index) {
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  // exchange of card which is below commented
-                  leading: CircleAvatar(
-                    radius: 30,
-                    // @@@ Container(
-                    // height: 60,
-                    // width: 60,
-                    // decoration: const BoxDecoration(
-                    //   color: Colors.blue,
-                    //   shape: BoxShape.circle,
-                    // ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5),
-                      child: FittedBox(
-                          child: Text('\$${transactions[index].amount}')),
-                    ),
-                  ),
-                  title: Text(
-                    transactions[index].title,
-                    style: Theme.of(context).textTheme.headline5,
-                  ),
-                  subtitle: Text(
-                    DateFormat.yMMMd().format(transactions[index].date),
-                  ),
-                  trailing: MediaQuery.of(context).size.width > 460
-                      ? TextButton(
-                          child: TextButton.icon(
-                            icon: const Icon(
-                              Icons.delete,
-                              color: Colors.red,
-                            ),
-                            label: const Text(
-                              "Delete",
-                              style: TextStyle(
-                                color: Colors.red,
-                              ),
-                            ),
-                            onPressed: () {
-                              deleteTransaction(transactions[index].id);
-                            },
-                          ),
-                          onPressed: () =>
-                              deleteTransaction(transactions[index].id),
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.delete),
-                          iconSize: 30,
-                          color: Colors.red,
-                          onPressed: () =>
-                              deleteTransaction(transactions[index].id),
-                        ),
-                ),
-              );
+              return TransactionItem(
+                  transaction: transactions[index],
+                  deleteTransaction: deleteTransaction);
             },
             itemCount: transactions.length,
           );
   }
 }
+
 
 // Card(
 //                     color: Colors.white70,
