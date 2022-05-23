8878e41647bf7cc2cc89e65ca91cfae70779aeb6
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-24 00:18:10 +0600

    adding adaptive biutton for both ios and android

---
+++ expense_planner/lib/widgets/adaptive_button.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdaptiveButton extends StatelessWidget {
+  final String text;
+  final Function handler;
+
+  const AdaptiveButton(this.text, this.handler, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (Platform.isIOS) {
+      return CupertinoButton(
+        child: Text(
+          text,
+          style: const TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        onPressed: () => handler,
+      );
+    } else {
+      return TextButton(
+        child: const Text(
+          'Choose Date',
+          style: TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+        ),
+        onPressed: () => handler,
+      );
+    }
+  }
+}
