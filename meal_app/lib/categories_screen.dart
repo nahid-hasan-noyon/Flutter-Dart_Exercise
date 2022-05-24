0d72aac7eae8cfc63bb61d1e5b669103223171e8
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-24 15:19:38 +0600

    creating gridview for category

---
+++ meal_app/lib/categories_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: const [],
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+      ),
+    );
+  }
+}
