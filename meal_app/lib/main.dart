237d1705b1db3345d237fba5490e2c31a1acb152
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-24 16:33:44 +0600

    creating page for each category

--- meal_app/lib/main.dart
+++ meal_app/lib/main.dart
@@ -15,7 +15,23 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+
+        // textTheme: ThemeData.light().textTheme.copyWith(
+        //       bodyText1: const TextStyle(
+        //         color: Color.fromRGBO(20, 51, 51, 1),
+        //       ),
+        //       bodyText2: const TextStyle(
+        //         color: Color.fromRGBO(20, 51, 51, 1),
+        //       ),
+        //       titleLarge: const TextStyle(
+        //         fontSize: 24,
+        //         fontFamily: 'RobotoCondensed',
+        //       ),
+        //     ),
+        // colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+        //     .copyWith(secondary: Colors.amber),
       ),
       home: const MyHomePage(title: 'DeliMeals'),
     );
