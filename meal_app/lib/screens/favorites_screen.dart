c744b6f78be8d558077a6de5a185d4ef003a17b8
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-05-29 19:24:10 +0600

    displaying the favorite meals in favorite section.

--- meal_app/lib/screens/favorites_screen.dart
+++ meal_app/lib/screens/favorites_screen.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/Models/meal.dart';
+import 'package:meal_app/widgets/meal_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
-  List<Meal> favoriteMeals;
-  FavoritesScreen(this.favoriteMeals, {Key? key}) : super(key: key);
+  final List<Meal> favoriteMeals;
+  const FavoritesScreen(this.favoriteMeals, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('The favorites :o'),
-    );
+    return favoriteMeals.isEmpty
+        ? const Center(
+            child: Text('You have no favorites yet - start adding some! '),
+          )
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return MealItem(
+                id: favoriteMeals[index].id,
+                categories: favoriteMeals[index].categories,
+                duration: favoriteMeals[index].duration,
+                imageUrl: favoriteMeals[index].imageUrl,
+                ingredients: favoriteMeals[index].ingredients,
+                isGlutenFree: favoriteMeals[index].isGlutenFree,
+                isLactoseFree: favoriteMeals[index].isLactoseFree,
+                isVegan: favoriteMeals[index].isVegan,
+                isVegetarian: favoriteMeals[index].isVegetarian,
+                steps: favoriteMeals[index].steps,
+                title: favoriteMeals[index].title,
+                affordability: favoriteMeals[index].affordability,
+                complexity: favoriteMeals[index].complexity,
+              );
+            },
+            itemCount: favoriteMeals.length,
+          );
   }
 }
