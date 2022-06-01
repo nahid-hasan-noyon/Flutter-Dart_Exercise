3d3e3fb94eb241916345c2ad5e4d17c60daa1cba
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-02 00:11:25 +0600

    adding routes

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/product_detail_screen.dart';
+import 'package:flutter_complete_guide/screens/products_overview_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,25 +8,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'MyShop',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop!'),
-      ),
+      initialRoute: ProductOverviewScreen.routeName,
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        ProductOverviewScreen.routeName: (ctx) => ProductOverviewScreen(),
+      },
     );
   }
 }
