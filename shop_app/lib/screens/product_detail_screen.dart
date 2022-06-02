18d23914bdea5b99fb75f02bde49a772113845d4
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-02 20:10:00 +0600

    using function to check product id, adding listen: false to prevent product detail screen from changing

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -9,10 +9,11 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String productId = ModalRoute.of(context).settings.arguments as String;
-    final loadedProduct =
-        Provider.of<ProductsProvider>(context).items.firstWhere(
-              (product) => product.id == productId,
-            );
+    final loadedProduct = Provider.of<ProductsProvider>(
+      context,
+      listen: false,
+      //? if any update comes when "listen: false" will prevent it from changing anything for now.
+    ).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
