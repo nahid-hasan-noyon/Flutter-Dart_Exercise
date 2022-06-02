6311fa693e6f56544ec0cdba21ecd46de05a8cbd
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-02 22:35:57 +0600

    converting to statefulWidget, adding a pop up small menu for toggle to favorite and all items.

--- shop_app/lib/screens/products_overview_screen.dart
+++ shop_app/lib/screens/products_overview_screen.dart
@@ -1,18 +1,53 @@
 import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
 import 'package:flutter_complete_guide/widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions { Favorites, All }
+
+class ProductsOverviewScreen extends StatefulWidget {
   static const routeName = '/product-overview';
 
-//* Dummy products
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showFavoritesOnly = false;
 
   @override
   Widget build(BuildContext context) {
+    // final productContainer =
+    //     Provider.of<ProductsProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showFavoritesOnly = true;
+                } else {
+                  _showFavoritesOnly = false;
+                }
+              });
+            },
+            itemBuilder: ((context) => [
+                  PopupMenuItem(
+                    child: Text('Only Favorites'),
+                    value: FilterOptions.Favorites,
+                  ),
+                  PopupMenuItem(
+                    child: Text('Show All'),
+                    value: FilterOptions.All,
+                  ),
+                ]),
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showFavoritesOnly),
     );
   }
 }
