1885028a35481e3d993d5a890411b734c40638da
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-02 17:39:05 +0600

    getting data through provider package

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/products_provider.dart';
+
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-details';
@@ -6,9 +9,13 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct =
+        Provider.of<ProductsProvider>(context).items.firstWhere(
+              (product) => product.id == productId,
+            );
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
