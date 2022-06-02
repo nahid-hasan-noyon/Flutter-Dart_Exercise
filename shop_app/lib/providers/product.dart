f92dbb9666c059d79a984e6b2bdb00110ff07233
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-02 20:10:59 +0600

    moving to provider folder

--- shop_app/lib/providers/product.dart
+++ shop_app/lib/providers/product.dart
@@ -1,4 +1,6 @@
-class Product {
+import 'package:flutter/material.dart';
+
+class Product with ChangeNotifier {
   String id;
   String title;
   String description;
@@ -14,4 +16,9 @@ class Product {
     this.isFavorite = false,
     this.price,
   });
+
+  void toggleFavoriteStatus() {
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
