447d18e256983843621d93a72370e2f0d4639c80
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-02 17:41:44 +0600

    adding changenotifierProvider from provider package to pass data in through root level and use only the elements who needs which.

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:flutter_complete_guide/screens/product_detail_screen.dart';
 import 'package:flutter_complete_guide/screens/products_overview_screen.dart';
+import 'package:flutter_complete_guide/providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => ProductsProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: ProductOverviewScreen.routeName,
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          ProductOverviewScreen.routeName: (ctx) => ProductOverviewScreen(),
+        },
       ),
-      initialRoute: ProductOverviewScreen.routeName,
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-        ProductOverviewScreen.routeName: (ctx) => ProductOverviewScreen(),
-      },
     );
   }
 }
