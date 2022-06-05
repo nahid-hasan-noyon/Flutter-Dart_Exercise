fa05be76f22454574d049c7d4819ffc65bfe2c50
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-05 23:50:42 +0600

    dividing cart in two files

---
+++ shop_app/lib/providers/cart_item.dart
@@ -0,0 +1,13 @@
+class CartItem {
+  String id;
+  String title;
+  int quantity;
+  double price;
+
+  CartItem({
+    this.id,
+    this.price,
+    this.quantity,
+    this.title,
+  });
+}
---
+++ shop_app/lib/providers/cart_items_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/cart_item.dart';
+
+class CartItemProvider with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  int get itemCount {
+    return _items.length;
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+}
