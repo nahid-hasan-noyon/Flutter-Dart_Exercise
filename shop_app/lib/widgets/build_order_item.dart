3870bfecb670bbd646725332ca38efa3f0bb3226
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-06 17:03:19 +0600

    adding widget for displaying ordered items, price and quantity

---
+++ shop_app/lib/widgets/build_order_item.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/orders.dart';
+import 'package:intl/intl.dart';
+
+class BuildOrderItem extends StatefulWidget {
+  final OrderItem order;
+
+  BuildOrderItem(this.order);
+
+  @override
+  State<BuildOrderItem> createState() => _BuildOrderItemState();
+}
+
+class _BuildOrderItemState extends State<BuildOrderItem> {
+  var _expanded = false;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(children: [
+        ListTile(
+          title: Text('\$${widget.order.amount}'),
+          subtitle: Text(
+            DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+          ),
+          trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                // todo
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              }),
+        ),
+        if (_expanded)
+          Container(
+            height: min(widget.order.products.length * 20.0 + 20, 180),
+            child: ListView(
+              children: widget.order.products
+                  .map(
+                    (prod) => Row(
+                      children: [
+                        Text(
+                          prod.title,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Spacer(),
+                        Text(
+                          '${prod.quantity}x \$${prod.price}',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                  .toList(),
+            ),
+          )
+      ]),
+    );
+  }
+}
