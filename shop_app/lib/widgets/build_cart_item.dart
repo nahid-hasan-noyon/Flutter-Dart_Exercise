bfc6ae677809fab7f006c83688e3b32e1cb20685
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-06 09:32:54 +0600

    adding widget to delete product form cart on swipe to left.

--- shop_app/lib/widgets/build_cart_item.dart
+++ shop_app/lib/widgets/build_cart_item.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_complete_guide/providers/cart_items_provider.dart';
 
 class BuildCartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
   BuildCartItem(
     this.id,
+    this.productId,
     this.price,
     this.quantity,
     this.title,
@@ -15,22 +19,41 @@ class BuildCartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text('\$${price}'),
+    return Dismissible(
+      direction: DismissDirection.endToStart,
+      key: ValueKey(id),
+      background: Container(
+        child: Icon(
+          Icons.delete,
+          size: 40,
+          color: Colors.white,
+        ),
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      ),
+      onDismissed: (dismiss) {
+        Provider.of<CartItemProvider>(context, listen: false)
+            .removeItem(productId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('\$${price}'),
+                ),
               ),
             ),
+            title: Text(title),
+            subtitle: Text('Total \$${(price * quantity)}'),
+            trailing: Text('${quantity}'),
           ),
-          title: Text(title),
-          subtitle: Text('Total \$${(price * quantity)}'),
-          trailing: Text('${quantity}'),
         ),
       ),
     );
