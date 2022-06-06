f8231e3cde5ab2c8b18d71501c3cb7a86bd77548
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-06 17:07:16 +0600

    adding appbar, appdrawer, and list of ordered items in ordered page.

---
+++ shop_app/lib/screens/orders_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/orders.dart';
+import 'package:flutter_complete_guide/widgets/app_drawer.dart';
+import 'package:flutter_complete_guide/widgets/build_order_item.dart';
+import 'package:provider/provider.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+  const OrdersScreen({key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+        automaticallyImplyLeading: true,
+      ),
+      drawer: AppDrawer(),
+      body: ListView.builder(
+        itemBuilder: (context, index) =>
+            BuildOrderItem(orderData.orders[index]),
+        itemCount: orderData.orders.length,
+      ),
+    );
+  }
+}
