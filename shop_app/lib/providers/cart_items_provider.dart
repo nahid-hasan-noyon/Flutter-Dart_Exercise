ca445347bde5bbd1b49d6aaa30985ff5f2105da7
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-07 09:27:43 +0600

    adding method of removing single item from cart

--- shop_app/lib/providers/cart_items_provider.dart
+++ shop_app/lib/providers/cart_items_provider.dart
@@ -50,6 +50,24 @@ class CartItemProvider with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (_items[productId].quantity > 1) {
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity - 1,
+        ),
+      );
+    } else if (_items.containsKey(productId)) {
+      _items.remove(productId);
+    } else
+      return;
+    notifyListeners();
+  }
+
   void clearCart() {
     _items = {};
     notifyListeners();
