7c115f62e5ee0cf1c1494f9f88309dac04197567
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-07 09:26:01 +0600

    giving more meaningful name

--- shop_app/lib/widgets/build_drawer.dart
+++ shop_app/lib/widgets/build_drawer.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/screens/orders_screen.dart';
+import 'package:flutter_complete_guide/screens/user_products_screen.dart';
 
-class AppDrawer extends StatelessWidget {
-  const AppDrawer({key}) : super(key: key);
+class BuildDrawer extends StatelessWidget {
+  const BuildDrawer({key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +24,20 @@ class AppDrawer extends StatelessWidget {
           ),
           Divider(),
           ListTile(
-            leading: Icon(Icons.shop),
+            leading: Icon(Icons.payment),
             title: Text('Orders'),
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(OrdersScreen.routeName);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage Products'),
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacementNamed(UserProductsScreen.routeName);
             },
           )
         ],
