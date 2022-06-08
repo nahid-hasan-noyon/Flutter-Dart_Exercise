1948fcc8b136fca3dbf96cf0f2bc3b6dd8594fde
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-08 18:17:28 +0600

    refactored a order now button to class, adding loding circle, load order products.

--- shop_app/lib/screens/cart_screen.dart
+++ shop_app/lib/screens/cart_screen.dart
@@ -30,19 +30,7 @@ class CartScreen extends StatelessWidget {
                   Chip(
                     label: Text('\$${cart.totalAmount.toStringAsFixed(2)}'),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      if (cart.totalAmount != 0.00) {
-                        Provider.of<Orders>(context, listen: false).addOrder(
-                          cart.items.values.toList(),
-                          cart.totalAmount,
-                        );
-                      }
-
-                      cart.clearCart();
-                    },
-                    child: Text('Order Now'),
-                  ),
+                  OrderButton(cart: cart),
                 ]),
           ),
         ),
@@ -62,6 +50,47 @@ class CartScreen extends StatelessWidget {
           ),
         )
       ]),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final CartItemProvider cart;
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: (widget.cart.totalAmount <= 0 || _isLoading)
+          ? null
+          : () async {
+              setState(() {
+                _isLoading = true;
+              });
+              if (widget.cart.totalAmount != 0.00) {
+                await Provider.of<Orders>(context, listen: false).addOrder(
+                  widget.cart.items.values.toList(),
+                  widget.cart.totalAmount,
+                );
+                setState(() {
+                  _isLoading = false;
+                });
+              }
+
+              widget.cart.clearCart();
+            },
+      child: _isLoading ? CircularProgressIndicator() : Text('Order Now'),
     );
   }
 }
