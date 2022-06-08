f29877219cc11f3355efae2b758953707e6aa97c
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-08 15:25:26 +0600

    adding editing product path and deleting icon and also show if failed to delete product.

--- shop_app/lib/widgets/build_user_product_item.dart
+++ shop_app/lib/widgets/build_user_product_item.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/products_provider.dart';
+import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
+import 'package:provider/provider.dart';
 
 class BuildUserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
-  BuildUserProductItem(this.title, this.imageUrl);
+  BuildUserProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final SMC = ScaffoldMessenger.of(context);
     return Card(
       child: ListTile(
         title: Text(title),
@@ -18,12 +23,29 @@ class BuildUserProductItem extends StatelessWidget {
           width: 100,
           child: Row(children: [
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
+              },
               icon: Icon(Icons.edit),
               color: Colors.grey,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () async {
+                try {
+                  await Provider.of<ProductsProvider>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (error) {
+                  SMC.showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        'Fail to delete item',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  );
+                }
+              },
               icon: Icon(Icons.delete),
               color: Colors.red,
             )
