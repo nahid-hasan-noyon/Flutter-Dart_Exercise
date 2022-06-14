f41ef0abb35c3c0a904ea65d453e5dd19e573295
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-14 23:42:05 +0600

    passing value from one class to another class and adding future builder.

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/splash-screen.dart';
+import 'package:provider/provider.dart';
+
 import 'package:flutter_complete_guide/providers/auth.dart';
-import 'package:flutter_complete_guide/providers/cart_items_provider.dart';
 import 'package:flutter_complete_guide/providers/orders.dart';
+import 'package:flutter_complete_guide/providers/products_provider.dart';
+import 'package:flutter_complete_guide/providers/cart_items_provider.dart';
 import 'package:flutter_complete_guide/screens/auth_screen.dart';
 import 'package:flutter_complete_guide/screens/cart_screen.dart';
 import 'package:flutter_complete_guide/screens/edit_product_screen.dart';
 import 'package:flutter_complete_guide/screens/orders_screen.dart';
 import 'package:flutter_complete_guide/screens/user_products_screen.dart';
-import 'package:provider/provider.dart';
-
 import 'package:flutter_complete_guide/screens/product_detail_screen.dart';
 import 'package:flutter_complete_guide/screens/products_overview_screen.dart';
-import 'package:flutter_complete_guide/providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,14 +24,18 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (ctx) => Auth(),
         ),
-        ChangeNotifierProvider(
-          create: (ctx) => ProductsProvider(),
+        ChangeNotifierProxyProvider<Auth, ProductsProvider>(
+          create: (_) => ProductsProvider(),
+          update: (_, auth, previousProducts) =>
+              previousProducts..assignTokenAndUserId(auth.token, auth.userId),
+        ),
+        ChangeNotifierProxyProvider<Auth, Orders>(
+          create: (_) => Orders(),
+          update: (_, auth, orders) =>
+              orders..setTokenAndUserId(auth.token, auth.userId),
         ),
         ChangeNotifierProvider(
           create: (ctx) => CartItemProvider(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => Orders(),
         ),
       ],
       child: Consumer<Auth>(
@@ -40,7 +45,16 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: authData.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+          home: authData.isAuth
+              ? ProductsOverviewScreen()
+              : FutureBuilder(
+                  future: authData.tryAutoLogin(),
+                  builder: (context, authResultSnapshot) =>
+                      authResultSnapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen(),
+                ),
           routes: {
             ProductsOverviewScreen.routeName: (ctx) => ProductsOverviewScreen(),
             ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
