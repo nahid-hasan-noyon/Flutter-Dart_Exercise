c31801b2e1520190c4a9204702685e1fc25fc5ab
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-14 23:39:24 +0600

    adding logout option in drawer

--- shop_app/lib/widgets/build_drawer.dart
+++ shop_app/lib/widgets/build_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/auth.dart';
 import 'package:flutter_complete_guide/screens/orders_screen.dart';
 import 'package:flutter_complete_guide/screens/user_products_screen.dart';
+import 'package:provider/provider.dart';
 
 class BuildDrawer extends StatelessWidget {
   const BuildDrawer({key}) : super(key: key);
@@ -39,7 +41,19 @@ class BuildDrawer extends StatelessWidget {
               Navigator.of(context)
                   .pushReplacementNamed(UserProductsScreen.routeName);
             },
-          )
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pop();
+              Navigator.of(context)
+                  .pushReplacementNamed('/'); //? goes to "home:" route
+              //? above one is to close the drawer.
+              Provider.of<Auth>(context, listen: false).logout();
+            },
+          ),
         ],
       ),
     );
