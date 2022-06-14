93553451484f80f459b11eba89a44e215e215bc9
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-14 23:20:49 +0600

    adding mothod for getting token and userid, change url format, fixing spelling mistake,

--- shop_app/lib/providers/orders.dart
+++ shop_app/lib/providers/orders.dart
@@ -19,26 +19,34 @@ class OrderItem {
 
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
+  String authToken;
+  String userId;
+
+  void setTokenAndUserId(String token, String id) {
+    authToken = token;
+    userId = id;
+    notifyListeners();
+  }
 
   List<OrderItem> get orders {
     return [..._orders];
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    final url =
-        Uri.https('shop-app-d68a8-default-rtdb.firebaseio.com', '/orders.json');
+    final url = Uri.parse(
+        'https://shop-app-d68a8-default-rtdb.firebaseio.com/orders/$userId.json?auth=$authToken');
     final timeStamp = DateTime.now();
     final response = await http.post(
       url,
       body: json.encode({
         'amount': total,
-        'dataTime': timeStamp.toIso8601String(),
+        'dateTime': timeStamp.toIso8601String(),
         'products': cartProducts
             .map((cp) => {
                   'id': cp.id,
                   'title': cp.title,
                   'quantity': cp.quantity,
-                  'price': cp.quantity,
+                  'price': cp.price,
                 })
             .toString(),
       }),
@@ -56,37 +64,35 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> fetchAndSetOrders() async {
-    final url =
-        Uri.https('shop-app-d68a8-default-rtdb.firebaseio.com', '/orders.json');
-    try {
-      final response = await http.get(url);
-      final extractedData = json.decode(response.body) as Map<String, dynamic>;
-      final List<OrderItem> loadedProducts = [];
-      if (extractedData != null) {
-        extractedData.forEach((orderId, orderData) {
-          loadedProducts.add(
-            OrderItem(
-              id: orderId,
-              amount: orderData['amount'],
-              dateTime: DateTime.parse(orderData['dataTime']),
-              products: (orderData['products'] as List<dynamic>)
-                  .map(
-                    (item) => CartItem(
-                      id: item['id'],
-                      price: item['price'],
-                      quantity: item['quantity'],
-                      title: item['title'],
-                    ),
-                  )
-                  .toList(),
-            ),
-          );
-        });
-        _orders = loadedProducts.reversed.toList();
-        notifyListeners();
-      }
-    } catch (error) {
-      throw (error);
+    final url = Uri.parse(
+        'https://shop-app-d68a8-default-rtdb.firebaseio.com/orders/$userId.json?auth=$authToken');
+    // try {
+    final response = await http.get(url);
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    final List<OrderItem> loadedOrders = [];
+    if (extractedData == null) {
+      return;
     }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                  id: item['id'],
+                  price: item['price'],
+                  quantity: item['quantity'],
+                  title: item['title'],
+                ),
+              )
+              .toList(),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
   }
 }
