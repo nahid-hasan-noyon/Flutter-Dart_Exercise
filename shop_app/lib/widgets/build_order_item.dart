fed44667db5e46573f61176d02790c575b0e1d23
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-14 23:40:04 +0600

    tweaking some places

--- shop_app/lib/widgets/build_order_item.dart
+++ shop_app/lib/widgets/build_order_item.dart
@@ -19,49 +19,51 @@ class _BuildOrderItemState extends State<BuildOrderItem> {
   Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.all(10),
-      child: Column(children: [
-        ListTile(
-          title: Text('\$${widget.order.amount}'),
-          subtitle: Text(
-            DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
-          ),
-          trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                // todo
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              }),
-        ),
-        if (_expanded)
-          Container(
-            height: min(widget.order.products.length * 20.0 + 20, 180),
-            child: ListView(
-              children: widget.order.products
-                  .map(
-                    (prod) => Row(
-                      children: [
-                        Text(
-                          prod.title,
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        Spacer(),
-                        Text(
-                          '${prod.quantity}x \$${prod.price}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                  .toList(),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
             ),
-          )
-      ]),
+            trailing: IconButton(
+                icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+                onPressed: () {
+                  // todo
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                }),
+          ),
+          if (_expanded)
+            Container(
+              height: min(widget.order.products.length * 20.0 + 20, 180),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (prod) => Row(
+                        children: [
+                          Text(
+                            prod.title,
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Spacer(),
+                          Text(
+                            '${prod.quantity}x \$${prod.price}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
+        ],
+      ),
     );
   }
 }
