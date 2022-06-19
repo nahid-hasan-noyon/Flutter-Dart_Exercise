acbfe210d3c32fbb9a67a0449588a94fae1ba5b5
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-20 00:37:39 +0600

    home page where list of favorite places can be seen

---
+++ favorite_places/lib/screens/places_list_screen.dart
@@ -0,0 +1,27 @@
+import 'package:favorite_places/screens/add_place_screen.dart';
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  static const routeName = '/';
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Favorite Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
