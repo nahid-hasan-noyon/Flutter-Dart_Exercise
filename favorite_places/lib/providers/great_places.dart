b489d6d8fc6253956fda93dd056e88dff1df459b
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-25 09:44:59 +0600

    place adding and fetching data method

--- favorite_places/lib/providers/great_places.dart
+++ favorite_places/lib/providers/great_places.dart
@@ -1,10 +1,45 @@
+import 'dart:io';
+
+import 'package:favorite_places/helpers/db_helper.dart';
 import 'package:favorite_places/models/place.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class GreatPlaces with ChangeNotifier {
-  final List<Place> _items = [];
+  List<Place> _items = [];
 
   List<Place> get items {
     return [..._items];
+  }
+
+  void addPlace(String pickedTitle, File pickedImage) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      location: null,
+      title: pickedTitle,
+    );
+    _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final datalist = await DBHelper.getData('user_places');
+
+    _items = datalist
+        .map(
+          (item) => Place(
+            id: item['id'] as String,
+            image: File(item['image'] as String),
+            title: item['title'] as String,
+            location: null,
+          ),
+        )
+        .toList();
+    notifyListeners();
   }
 }
