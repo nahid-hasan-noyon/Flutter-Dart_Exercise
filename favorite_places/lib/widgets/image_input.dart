641993c7603ba546766c1b9be51068c52ab72ff9
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-06-25 09:48:06 +0600

    adding image input widget which will be stored in device memory.

---
+++ favorite_places/lib/widgets/image_input.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as sysPath;
+
+class ImageInput extends StatefulWidget {
+  const ImageInput(this.onSelectImage, {Key? key}) : super(key: key);
+
+  final Function onSelectImage;
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File? _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    if (imageFile == null) {
+      return;
+    }
+    setState(() {
+      _storedImage = File(imageFile.path); //imageFile as File;
+    });
+
+    final appDir = await sysPath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = await _storedImage!.copy('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.blueGrey),
+          ),
+          alignment: Alignment.center,
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : const Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: TextButton.icon(
+            onPressed: () {
+              _takePicture();
+            },
+            icon: const Icon(Icons.camera),
+            label: const Text('Take Picture'),
+            style: ElevatedButton.styleFrom(
+              maximumSize: const Size(100, 50),
+              shadowColor: Colors.green,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
