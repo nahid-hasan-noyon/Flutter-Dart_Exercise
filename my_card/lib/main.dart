bdc7ca46c52ffb7edf46287291556e7178cfdbd7
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-09-23 21:41:40 +0600

    making a card containing image, name, number, email.

--- my_card/lib/main.dart
+++ my_card/lib/main.dart
@@ -10,9 +10,81 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+            ),
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/nahid.png'),
+              radius: 50.0,
+            ),
+            const Text(
+              'Nahid Hasan Noyon',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            const Text(
+              'Flutter Developer',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+              ),
+            ),
+            const SizedBox(
+              width: 300,
+              child: Divider(
+                thickness: 3,
+              ),
+            ),
+            const Card(
+              margin: EdgeInsets.all(10),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  size: 25,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '01627465928',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+            ),
+            const Card(
+              margin: EdgeInsets.all(10),
+              color: Colors.white,
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  size: 25,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'nahidhasannoyon20@gmail.com',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.teal,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
