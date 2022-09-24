ef2dfc88cf5aab76fa81bcc9a7a296cf0124ef2d
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-09-25 01:13:00 +0600

    a simple widget which will detect gesture and get color, custom widget and function as parameter.

---
+++ bmi_calculator/lib/widgets/reusable_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+  final Widget cardChild;
+  final Function onPress;
+
+  const ReusableCard({
+    required this.color,
+    required this.cardChild,
+    required this.onPress,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => onPress,
+        child: Container(
+          margin: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: color,
+          ),
+        ),
+      ),
+    );
+  }
+}
