c325e3ffd4b88b05506214c9494845869a48a410
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-09-24 20:43:39 +0600

    removing named parameters

--- quizzler/lib/model/question.dart
+++ quizzler/lib/model/question.dart
@@ -2,7 +2,7 @@ class Question {
   late String questionText;
   late bool questionAnswer;
 
-  Question({String? q, bool? a}) {
+  Question(String? q, bool? a) {
     questionText = q!;
     questionAnswer = a!;
   }
