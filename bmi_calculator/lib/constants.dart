d7f1804476404cfc26b4abd244c0cc36b3517541
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-09-27 20:41:11 +0600

    adding some constant value in separate file.

---
+++ bmi_calculator/lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  // color: Color(0xff8d8e98),
+  color: Colors.white,
+);
+const kNumberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+  color: Colors.white,
+);
+
+const kActiveCardColor = Colors.grey;
+// const kActiveCardColor = Color(0xff1d1e33);
+const kInactiveCardColor = Color(0xff111328);
+const kBottomContainerHeight = 60.0;
+const kBottomContainerColor = Color(0xFFEB1555);
