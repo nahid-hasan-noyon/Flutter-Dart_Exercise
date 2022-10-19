742e9b19726511b9ac750d8de9cf42e38d252f7d
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-19 20:52:46 +0600

    adding path of .en file

--- clima/lib/main.dart
+++ clima/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:clima/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
