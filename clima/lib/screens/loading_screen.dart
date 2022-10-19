bcfc03f343d306ea13a06a79be9b963363e6dc08
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-19 22:47:13 +0600

    adding the data fetch method

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,18 +1,47 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
   @override
-  _LoadingScreenState createState() => _LoadingScreenState();
+  LoadingScreenState createState() => LoadingScreenState();
 }
 
-class _LoadingScreenState extends State<LoadingScreen> {
+class LoadingScreenState extends State<LoadingScreen> {
+  double? latitude;
+  double? longitude;
+
+  @override
+  void initState() {
+    getGeoLocationData();
+    super.initState();
+  }
+
+  void getGeoLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    String? apiKey = dotenv.env['API_KEY'];
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+    print(weatherData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
+          onPressed: () async {
+            // fetchDataD();
+            getGeoLocationData();
           },
           child: const Text('Get Location'),
         ),
