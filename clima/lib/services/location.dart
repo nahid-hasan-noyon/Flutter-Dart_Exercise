f8787357592c2c957566321b83b1a3e5eaf2f245
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-19 22:46:00 +0600

    fixing the spelling mistake

--- clima/lib/services/location.dart
+++ clima/lib/services/location.dart
@@ -28,7 +28,7 @@ class Location {
       Position position = await Geolocator.getCurrentPosition(
           desiredAccuracy: LocationAccuracy.low);
       latitude = position.latitude;
-      longitude = position.latitude;
+      longitude = position.longitude;
       // print(position);
     } catch (e) {
       print(e);
