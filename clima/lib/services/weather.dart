82e6bc6fe0ae3a3120dbb1702d9e2a70c22ffeb0
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-19 23:53:37 +0600

    refactoring and adding city weather method

--- clima/lib/services/weather.dart
+++ clima/lib/services/weather.dart
@@ -1,4 +1,30 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class WeatherModel {
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    String? apiKey = dotenv.env['API_KEY'];
+    String? weatherUrl = dotenv.env['OpenWeatherMap_URL'];
+    NetworkHelper networkHelper = NetworkHelper(
+        '$weatherUrl?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getCityWeather(String cityName) async {
+    String? apiKey = dotenv.env['API_KEY'];
+    String? weatherUrl = dotenv.env['OpenWeatherMap_URL'];
+    NetworkHelper networkHelper =
+        NetworkHelper('$weatherUrl?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
