5207f38ace98d8bab53a3bdcaabf850937578357
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-24 20:25:08 +0600

    adding text field to add task

---
+++ todoey/lib/screens/add_task_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xff757575),
+      child: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            children: const [
+              Text(
+                'Add Task',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                  fontSize: 30,
+                ),
+              ),
+              TextField(
+                autofocus: true,
+                textAlign: TextAlign.center,
+              ),
+              ElevatedButton(onPressed: null, child: Text('Add'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
