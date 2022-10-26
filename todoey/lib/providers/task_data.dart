430c5d89aae061c6055aab063a568d87256cc7be
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-10-26 23:49:38 +0600

    adding new data provider

---
+++ todoey/lib/providers/task_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:todoey/modals/task.dart';
+
+class TaskData extends ChangeNotifier {
+  List<Task> tasks = [
+    Task(name: 'first'),
+    Task(name: 'first2'),
+    Task(name: 'first3'),
+  ];
+}
