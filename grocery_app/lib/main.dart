f10ba788538a56d76795a70d4f92ebefe079a2b0
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-12-24 13:33:12 +0600

    adding package, provider and services for dark theme

--- grocery_app/lib/main.dart
+++ grocery_app/lib/main.dart
@@ -1,26 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:grocery_app/providers/dark_theme_provider.dart';
 import 'package:grocery_app/screens/home_screen.dart';
 import 'package:grocery_app/constants.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // * locking the orientation to portrait mode
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider darkThemeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    darkThemeProvider.setDarkTheme =
+        await darkThemeProvider.darkThemePrefs.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    bool isDarkTheme = true;
-    return MaterialApp(
-      theme: themeData(isDarkTheme, context),
-      home: const HomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          return darkThemeProvider;
+        })
+      ],
+      child:
+          Consumer<DarkThemeProvider>(builder: (context, themeProvider, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: themeData(themeProvider.getDarkTheme, context),
+          home: const HomeScreen(),
+        );
+      }),
     );
   }
 }
