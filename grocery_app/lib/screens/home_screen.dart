d78a377e4758157c09f8373dbb11094dc688c7a2
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-12-24 18:53:35 +0600

    removing theme selecting form home page and adding it to user page, adding more user related tiles

--- grocery_app/lib/screens/home_screen.dart
+++ grocery_app/lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:grocery_app/providers/dark_theme_provider.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,22 +10,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final themeState = Provider.of<DarkThemeProvider>(context);
-    return Scaffold(
-      body: Center(
-        child: SwitchListTile(
-          title: const Text('Theme'),
-          secondary: Icon(themeState.getDarkTheme
-              ? Icons.dark_mode_outlined
-              : Icons.light_mode_outlined),
-          onChanged: (bool value) {
-            setState(() {
-              themeState.setDarkTheme = value;
-            });
-          },
-          value: themeState.getDarkTheme,
-        ),
-      ),
-    );
+    return const Scaffold();
   }
 }
--- grocery_app/lib/screens/user_screen.dart
+++ grocery_app/lib/screens/user_screen.dart
@@ -1,14 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/providers/dark_theme_provider.dart';
+import 'package:provider/provider.dart';
 
-class UserScreen extends StatelessWidget {
+class UserScreen extends StatefulWidget {
   const UserScreen({super.key});
 
   @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('User Screen'),
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () => print('My name was pressed.'),
+                    child: const Text(
+                      'Hi,  My name',
+                      style: TextStyle(
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const Text('nahidhasannoyon20@gmail.com'),
+                  const Divider(
+                    height: 40,
+                    thickness: 2,
+                  ),
+                  _listTiles(
+                    icon: Icons.manage_accounts_outlined,
+                    title: "Address",
+                    subtitle: "My Address",
+                    onPress: () {},
+                  ),
+                  _listTiles(
+                    icon: Icons.shopping_bag_outlined,
+                    title: "Order",
+                    onPress: () {},
+                  ),
+                  _listTiles(
+                    icon: Icons.favorite_border_outlined,
+                    title: "WishList",
+                    onPress: () {},
+                  ),
+                  _listTiles(
+                    icon: Icons.remove_red_eye_outlined,
+                    title: "Viewed",
+                    onPress: () {},
+                  ),
+                  _listTiles(
+                    icon: Icons.password_outlined,
+                    title: "Forgot Password",
+                    onPress: () {},
+                  ),
+                  SwitchListTile(
+                    title: const Text(
+                      'Theme',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    secondary: Icon(themeState.getDarkTheme
+                        ? Icons.dark_mode_outlined
+                        : Icons.light_mode_outlined),
+                    onChanged: (bool value) {
+                      setState(() {
+                        themeState.setDarkTheme = value;
+                      });
+                    },
+                    value: themeState.getDarkTheme,
+                  ),
+                  _listTiles(
+                    icon: Icons.exit_to_app_outlined,
+                    title: "Logout",
+                    onPress: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _listTiles({
+    required IconData icon,
+    required String title,
+    String? subtitle,
+    required Function onPress,
+  }) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      subtitle: Text(subtitle ?? ""),
+      trailing: const Icon(Icons.arrow_forward_ios_outlined),
+      onTap: (() {
+        onPress();
+      }),
     );
   }
 }
