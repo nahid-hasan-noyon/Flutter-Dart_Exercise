c95fd8dcc52555cb2c3b714a9ca32d95edd7d4fa
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-12-25 00:13:46 +0600

    adding category in categories screen

--- grocery_app/lib/screens/categories_screen.dart
+++ grocery_app/lib/screens/categories_screen.dart
@@ -5,9 +5,46 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Categories Screen'),
+    return SafeArea(
+      child: Scaffold(
+        body: InkWell(
+          onTap: () {
+            // todo navigate to another page
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red.withOpacity(0.1),
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: Colors.red.withOpacity(0.7),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.3,
+                  width: MediaQuery.of(context).size.width * 0.3,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        'https://img.freepik.com/free-photo/healthy-vegetables-wooden-table_1150-38014.jpg',
+                      ),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Category Name',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
