a2a0bbd5f76113a527c594df772f37c0da1d541f
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2022-12-26 12:46:41 +0600

    adding slider for offered products

--- grocery_app/lib/screens/home_screen.dart
+++ grocery_app/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_app/widgets/on_sale_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,46 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .3,
+            child: Swiper(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Image.network(
+                  'https://img.freepik.com/free-photo/healthy-vegetables-wooden-table_1150-38014.jpg',
+                  fit: BoxFit.fill,
+                );
+              },
+              autoplay: true,
+              pagination: const SwiperPagination(
+                alignment: Alignment.bottomCenter,
+                builder: DotSwiperPaginationBuilder(
+                  color: Colors.white,
+                  activeColor: Colors.red,
+                ),
+              ),
+              control: const SwiperControl(),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+            width: MediaQuery.of(context).size.width * 0.95,
+            child: ListView.builder(
+              itemCount: 5,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return const OnSaleWidget();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
