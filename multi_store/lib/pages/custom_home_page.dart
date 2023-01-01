1431898b7e27e1674c0b4de34a556f6dabdfb9aa
nahid-hasan-noyon <nahidhasannoyon20@gmail.com>
2023-01-01 20:03:04 +0600

    adding homepage and home tab

---
+++ multi_store/lib/pages/custom_home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import '../tabs/home_tab.dart';
+
+class CustomHomePage extends StatefulWidget {
+  const CustomHomePage({super.key});
+
+  @override
+  State<CustomHomePage> createState() => _CustomHomePageState();
+}
+
+class _CustomHomePageState extends State<CustomHomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _tabs = [
+    const HomeTab(),
+    const Center(
+      child: Text('Category Tab'),
+    ),
+    const Center(
+      child: Text('Stores Tab'),
+    ),
+    const Center(
+      child: Text('Cart Tab'),
+    ),
+    const Center(
+      child: Text('Profile Tab'),
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.w600,
+        ),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Category'),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'Stores'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart), label: 'Cart'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
